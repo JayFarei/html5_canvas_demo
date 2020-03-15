2e738393d0cc6666e7a952f867ec094cbf197b24
Gabriele Farei <JayFarei@users.noreply.github.com>
2020-03-15 19:28:07 +0000

    refactoring, reformatting

--- js/collision.js
+++ js/collision.js
@@ -1,14 +1,12 @@
 // SETTING UP CANVAS
 
 // searching HTML doc for HTML doc = canvas and assign it
-var canvas = document.querySelector('canvas');
+var canvas = document.querySelector("canvas");
 
 // loading functions to draw in variable c
-var c = canvas.getContext('2d');
-
+var c = canvas.getContext("2d");
 
 // DEFINING RESPONSIVE BEHAVIOUR
-
 
 // canvas size to dynamically adjust with the page
 
@@ -17,31 +15,27 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // calling the canvas adjustment every time the window is resized
-window.addEventListener('resize',
-    function(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        
-        // reset circle generation on resize
-        init();
-    }
-)
+window.addEventListener("resize", function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
+  // reset circle generation on resize
+  init();
+});
 
 // INTERACTIVITY INPUTS
 
 // interaction using event listener
 
 var mouse = {
-    x: undefined, y: undefined
-}
+  x: undefined,
+  y: undefined
+};
 
-window.addEventListener('mousemove', 
-function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
+window.addEventListener("mousemove", function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
 });
-
 
 // VARIABLES FOR THE ANIMATION
 
@@ -52,80 +46,72 @@ var randomSizeMultiple = 5;
 var numberCircles = 200;
 var speedMultiple = 10;
 // var area = 50;
-var colorArray = [
-    '#DD614A',
-    '#F48668',
-    '#F4A698',
-    '#C5C392',
-    '#73A580',
-];
-
-
+var colorArray = ["#DD614A", "#F48668", "#F4A698", "#C5C392", "#73A580"];
 
 // Random colour generator
 function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)]
-  }
-
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
 // OBJECTS IN THE CANVAS
 
 // creating a javascript object for circles
-function Circle(x, y, velocity, radius,mass) {
-    this.x = x;
-    this.y = y;
-    this.velocity = velocity;
-    this.radius = radius;
-    this.colour = randomColor(colorArray)
-    this.mass = mass; // hardcoded mass for elastic collision function
+function Circle(x, y, velocity, radius, mass) {
+  this.x = x;
+  this.y = y;
+  this.velocity = velocity;
+  this.radius = radius;
+  this.colour = randomColor(colorArray);
+  this.mass = mass; // hardcoded mass for elastic collision function
 
-    // drawing the circle
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,Math.PI * 2, false);
-        c.fillStyle = this.colour;
-        c.fill()
+  // drawing the circle
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.colour;
+    c.fill();
+  };
+
+  // update function for every frame
+  this.update = function() {
+    // COLLISION DETECTION
+
+    // Walls of the canvas
+    // setting bounce back via negative velocity when reaches borders
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.velocity.x = -this.velocity.x;
     }
 
-    // update function for every frame
-    this.update = function() {
-
-        // COLLISION DETECTION
-
-        // Walls of the canvas
-        // setting bounce back via negative velocity when reaches borders
-        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
-            this.velocity.x = -this.velocity.x;
-        }
-    
-        if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.velocity.y = -this.velocity.y;
-        }
-    
-        // this is the commands that triggers the change of frame with delta = dx/dy
-        this.x += this.velocity.x;
-        this.y += this.velocity.y;
-        
-        // Other circles
-
-        for (let i = 0; i < circleArray.length; i++) {
-            if(this === circleArray[i]) {
-                continue }; // ensuring I am not calculating collision with itself
-            if(getDistance(this.x,this.y,circleArray[i].x,circleArray[i].y) 
-                - this.radius - circleArray[i].radius < 0 ) {
-                    console.log ("frame collisions"); // printing collision detection
-                    resolveCollision(this, circleArray[i]); // computing new velocity upon collision
-                }
-
-        }
-
-        this.draw();
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.velocity.y = -this.velocity.y;
     }
 
+    // this is the commands that triggers the change of frame with delta = dx/dy
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+
+    // Other circles
+
+    for (let i = 0; i < circleArray.length; i++) {
+      if (this === circleArray[i]) {
+        continue;
+      } // ensuring I am not calculating collision with itself
+      if (
+        getDistance(this.x, this.y, circleArray[i].x, circleArray[i].y) -
+          this.radius -
+          circleArray[i].radius <
+        0
+      ) {
+        console.log("frame collisions"); // printing collision detection
+        resolveCollision(this, circleArray[i]); // computing new velocity upon collision
+      }
+    }
+
+    this.draw();
+  };
 }
 
-
-// ELASTIC COLLISION FUNCTION 
+// ELASTIC COLLISION FUNCTION
 /**
  * Rotates coordinate system for velocities
  *
@@ -137,12 +123,12 @@ function Circle(x, y, velocity, radius,mass) {
  */
 
 function rotate(velocity, angle) {
-    const rotatedVelocities = {
-        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-    };
+  const rotatedVelocities = {
+    x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+    y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+  };
 
-    return rotatedVelocities;
+  return rotatedVelocities;
 }
 
 /**
@@ -155,117 +141,119 @@ function rotate(velocity, angle) {
  */
 
 function resolveCollision(particle, otherParticle) {
-    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+  const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+  const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
 
-    const xDist = otherParticle.x - particle.x;
-    const yDist = otherParticle.y - particle.y;
+  const xDist = otherParticle.x - particle.x;
+  const yDist = otherParticle.y - particle.y;
 
-    // Prevent accidental overlap of particles
-    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+  // Prevent accidental overlap of particles
+  if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+    // Grab angle between the two colliding particles
+    const angle = -Math.atan2(
+      otherParticle.y - particle.y,
+      otherParticle.x - particle.x
+    );
 
-        // Grab angle between the two colliding particles
-        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+    // Store mass in var for better readability in collision equation
+    const m1 = particle.mass;
+    const m2 = otherParticle.mass;
 
-        // Store mass in var for better readability in collision equation
-        const m1 = particle.mass;
-        const m2 = otherParticle.mass;
+    // Velocity before equation
+    const u1 = rotate(particle.velocity, angle);
+    const u2 = rotate(otherParticle.velocity, angle);
 
-        // Velocity before equation
-        const u1 = rotate(particle.velocity, angle);
-        const u2 = rotate(otherParticle.velocity, angle);
+    // Velocity after 1d collision equation
+    const v1 = {
+      x: (u1.x * (m1 - m2)) / (m1 + m2) + (u2.x * 2 * m2) / (m1 + m2),
+      y: u1.y
+    };
+    const v2 = {
+      x: (u2.x * (m1 - m2)) / (m1 + m2) + (u1.x * 2 * m2) / (m1 + m2),
+      y: u2.y
+    };
 
-        // Velocity after 1d collision equation
-        const v1 = { x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2), y: u1.y };
-        const v2 = { x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2), y: u2.y };
+    // Final velocity after rotating axis back to original location
+    const vFinal1 = rotate(v1, -angle);
+    const vFinal2 = rotate(v2, -angle);
 
-        // Final velocity after rotating axis back to original location
-        const vFinal1 = rotate(v1, -angle);
-        const vFinal2 = rotate(v2, -angle);
+    // Swap particle velocities for realistic bounce effect
+    particle.velocity.x = vFinal1.x;
+    particle.velocity.y = vFinal1.y;
 
-        // Swap particle velocities for realistic bounce effect
-        particle.velocity.x = vFinal1.x;
-        particle.velocity.y = vFinal1.y;
-
-        otherParticle.velocity.x = vFinal2.x;
-        otherParticle.velocity.y = vFinal2.y;
-    }
+    otherParticle.velocity.x = vFinal2.x;
+    otherParticle.velocity.y = vFinal2.y;
+  }
 }
 
-// CALCULATE DISTANCE BETWEEN TWO COORDINATES 
+// CALCULATE DISTANCE BETWEEN TWO COORDINATES
 
-function getDistance (x1,y1,x2,y2) {
-    let xDistance = x2 - x1;
-    let yDistance = y2 - y1;
+function getDistance(x1, y1, x2, y2) {
+  let xDistance = x2 - x1;
+  let yDistance = y2 - y1;
 
-    return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+  return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
 }
 
-
-
-// GENERATION OF ARRAY OF CIRCLES 
-// To re-generate circles on the resized window rather than expanding the existing one 
+// GENERATION OF ARRAY OF CIRCLES
+// To re-generate circles on the resized window rather than expanding the existing one
 
 var circleArray = [];
 
 function init() {
+  circleArray = [];
 
-    circleArray = [];
+  // generate new array of circles filling the bigger window size
+  for (var i = 0; i < numberCircles; i++) {
+    var radius = Math.random() * randomSizeMultiple + basisRadius;
+    // need to adjust for the radius to avoid them spawn outside of the canvas
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
 
-    // generate new array of circles filling the bigger window size
-    for ( var i = 0; i < numberCircles; i++){
-        var radius = Math.random() * randomSizeMultiple + basisRadius;
-        // need to adjust for the radius to avoid them spawn outside of the canvas
-        var x = Math.random() * (innerWidth-radius * 2) + radius;
-        var y = Math.random() * (innerHeight-radius * 2) + radius;
+    var velocity = {
+      x: (Math.random() - 0.5) * speedMultiple, // horizontal velocity
+      y: (Math.random() - 0.5) * speedMultiple // vertical velocity
+    };
 
-        var velocity = {
-            x: (Math.random() - 0.5) * speedMultiple, // horizontal velocity 
-            y: (Math.random() - 0.5) * speedMultiple // vertical velocity
+    var mass = 1;
+
+    // if generation overlaps - regenerate x and y coordinates
+    if (i != 0) {
+      for (let j = 0; j < circleArray.length; j++) {
+        if (
+          getDistance(x, y, circleArray[j].x, circleArray[j].y) -
+            radius -
+            circleArray[j].radius <
+          0
+        ) {
+          var x = Math.random() * (innerWidth - radius * 2) + radius;
+          var y = Math.random() * (innerHeight - radius * 2) + radius;
+
+          j = -1; // to get the loop to start again
         }
-
-        var mass = 1;
-    
-        // if generation overlaps - regenerate x and y coordinates
-        if (i != 0) {
-            for (let j = 0; j < circleArray.length; j++) {
-                if( getDistance(x,y,circleArray[j].x,circleArray[j].y) 
-                    - radius - circleArray[j].radius < 0 ) {
-                        var x = Math.random() * (innerWidth-radius * 2) + radius;
-                        var y = Math.random() * (innerHeight-radius * 2) + radius;
-                        
-                        j = -1; // to get the loop to start again
-
-                }
-                    
-            }
-
-        }
-
-        // NOTE:
-        // to instantiate a new object you need to set "new" in front
-        // var circle = new Circle(200,200,3,3,30);
-        circleArray.push(new Circle(x, y, velocity, radius,mass));
+      }
     }
 
+    // NOTE:
+    // to instantiate a new object you need to set "new" in front
+    // var circle = new Circle(200,200,3,3,30);
+    circleArray.push(new Circle(x, y, velocity, radius, mass));
+  }
 }
-
 
 // ANIMATION ON EACH FRAME
 
 // creating a function to create an animation loop
-function animate () {
-    // this is the framework function that calls for the next frame
-    requestAnimationFrame(animate);
-    // clearing the canvas each time we go through this loop to avoid "trail" effect
-    c.clearRect(0,0,innerWidth, innerHeight)
+function animate() {
+  // this is the framework function that calls for the next frame
+  requestAnimationFrame(animate);
+  // clearing the canvas each time we go through this loop to avoid "trail" effect
+  c.clearRect(0, 0, innerWidth, innerHeight);
 
-    // For every circle object in array - get me the next frame using the .update() fn of the obj.
-    for (var i = 0; i < circleArray.length; i++) {
-        circleArray[i].update(circleArray);
-    }
-
-
+  // For every circle object in array - get me the next frame using the .update() fn of the obj.
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update(circleArray);
+  }
 }
 
 // EXECUTING THE ANIMATION
--- js/interaction.js
+++ js/interaction.js
@@ -1,6 +1,5 @@
 // searching HTML doc for HTML doc = canvas and assign it
-var canvas = document.querySelector('canvas');
-
+var canvas = document.querySelector("canvas");
 
 // canvas size to dynamically adjust with the page
 
@@ -8,116 +7,99 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-
 // loading functions to draw in variable c
-var c = canvas.getContext('2d');
-
+var c = canvas.getContext("2d");
 
 // interaction using event listener
 
 var mouse = {
-    x: undefined, y: undefined
-}
+  x: undefined,
+  y: undefined
+};
 
-
-
-window.addEventListener('mousemove', 
-function(event){
-    mouse.x = event.x;
-    mouse.y = event.y;
+window.addEventListener("mousemove", function(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
 });
 
 // calling the canvas adjustment every time the window is resized
-window.addEventListener('resize',
-    function(){
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-        
-        // reset circle generation on resize
-        init();
-        this.console.log(circleArray);
-    }
-)
+window.addEventListener("resize", function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-
+  // reset circle generation on resize
+  init();
+  this.console.log(circleArray);
+});
 
 // defining interaction animation parameters
 var maxRadius = 30;
 var minRadius = 20;
 var area = 50;
 
-var colorArray = [
-    '#cc5803',
-    '#e2711d',
-    '#ff9595',
-    '#ffb627',
-    '#ffc971',
-];
+var colorArray = ["#cc5803", "#e2711d", "#ff9595", "#ffb627", "#ffc971"];
 
 // Random colour generator
 function randomColor(colors) {
-    return colors[Math.floor(Math.random() * colors.length)]
-  }
-
+  return colors[Math.floor(Math.random() * colors.length)];
+}
 
 // creating a javascript object for circles
 function Circle(x, y, dx, dy, radius) {
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.radius = radius;
-    this.minRadius = radius;
-    this.colour = randomColor(colorArray)
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  this.minRadius = radius;
+  this.colour = randomColor(colorArray);
 
-    // drawing the circle
-    this.draw = function() {
-        c.beginPath();
-        c.arc(this.x,this.y,this.radius,0,Math.PI * 2, false);
-        c.fillStyle = this.colour;
-        c.fill()
+  // drawing the circle
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.colour;
+    c.fill();
+  };
+
+  // update function for every frame
+  this.update = function() {
+    // whenever the mouse position is within "area" from the circle - increase radius
+    if (
+      mouse.x - this.x < area &&
+      mouse.x - this.x > -area &&
+      mouse.y - this.y < area &&
+      mouse.y - this.y > -area
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius -= 1;
     }
 
-    // update function for every frame
-    this.update = function() {
-
-        // whenever the mouse position is within "area" from the circle - increase radius
-        if(mouse.x - this.x < area && mouse.x - this.x > -area && mouse.y - this.y < area && mouse.y - this.y > -area) {
-            if (this.radius < maxRadius) {
-            this.radius +=1;
-        }
-    
-        } else if (this.radius > this.minRadius) {
-        this.radius -=1;
-        }
-
-        // setting bounce back via negative velocity when reaches borders
-        if(this.x + this.radius > innerWidth || this.x - this.radius < 0){
-            this.dx = -this.dx;
-        }
-    
-        if(this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-            this.dy = -this.dy;
-        }
-    
-        // this is the commands that triggers the change of frame with delta = dx/dy
-        this.x += this.dx;
-        this.y += this.dy;
-        
-
-        this.draw();
+    // setting bounce back via negative velocity when reaches borders
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
     }
 
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+    }
+
+    // this is the commands that triggers the change of frame with delta = dx/dy
+    this.x += this.dx;
+    this.y += this.dy;
+
+    this.draw();
+  };
 }
 
-
-// // OPTION 1: 
+// // OPTION 1:
 // // Removing the init() function from startup and resize generated an effect that allows circles to expand in the new window size rather than regenerate
-
 
 // // creating an array to store the circles that will be generated
 // var circleArray = [];
-
 
 // // generating an array if circle objects
 // for(var i = 0; i <800; i++){
@@ -132,62 +114,44 @@ function Circle(x, y, dx, dy, radius) {
 //     circleArray.push(new Circle(x, y, dx, dy, radius));
 // }
 
-
-
 // OPTIONAL:
-// To re-generate circles on the resized window rather than expanding the existing one 
+// To re-generate circles on the resized window rather than expanding the existing one
 
 var circleArray = [];
 
 function init() {
+  // resetting the array
+  circleArray = [];
 
-    // resetting the array
-    circleArray = [];
+  // generate new array of circles filling the bigger window size
+  for (var i = 0; i < 800; i++) {
+    var radius = Math.random() * 3 + 1;
+    // need to adjust for the radius to avoid them spawn outside of the canvas
+    var x = Math.random() * (innerWidth - radius * 2) + radius;
+    var y = Math.random() * (innerHeight - radius * 2) + radius;
+    var dx = (Math.random() - 0.5) * 5; // horizontal velocity
+    var dy = (Math.random() - 0.5) * 5; // vertical velocity
 
-    // generate new array of circles filling the bigger window size
-    for(var i = 0; i <800; i++){
-        var radius = Math.random()* 3 + 1;
-        // need to adjust for the radius to avoid them spawn outside of the canvas
-        var x = Math.random() * (innerWidth-radius * 2) + radius;
-        var y = Math.random() * (innerHeight-radius * 2) + radius;
-        var dx = (Math.random() - 0.5) * 5; // horizontal velocity
-        var dy = (Math.random() - 0.5) * 5; // vertical velocity
-    
-        // on every iteration I push the circle in
+    // on every iteration I push the circle in
 
-    
-        // NOTE:
-        // to instantiate a new object you need to set "new" in front
-        // var circle = new Circle(200,200,3,3,30);
-        circleArray.push(new Circle(x, y, dx, dy, radius));
-    }
-
+    // NOTE:
+    // to instantiate a new object you need to set "new" in front
+    // var circle = new Circle(200,200,3,3,30);
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
 // creating a function to create an animation loop
-function animate () {
-    // this is the framework function that calls for the next frame
-    requestAnimationFrame(animate);
-    // clearing the canvas each time we go through this loop to avoid "trail" effect
-    c.clearRect(0,0,innerWidth, innerHeight)
+function animate() {
+  // this is the framework function that calls for the next frame
+  requestAnimationFrame(animate);
+  // clearing the canvas each time we go through this loop to avoid "trail" effect
+  c.clearRect(0, 0, innerWidth, innerHeight);
 
-    // For every circle object in array - get me the next frame using the .update() fn of the obj.
-    for (var i = 0; i < circleArray.length; i++) {
-        circleArray[i].update();
-        
-    }
-
+  // For every circle object in array - get me the next frame using the .update() fn of the obj.
+  for (var i = 0; i < circleArray.length; i++) {
+    circleArray[i].update();
+  }
 }
 
 init();
